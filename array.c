c351c09358b821047f77e94b9f2a58ec275943c8
Rudra150304 <rudra.p.s15032004@gmail.com>
2024-10-04 18:12:52 +0530

    Comments added

--- array.c
+++ array.c
@@ -2,13 +2,15 @@
 int main()
 {int a[30],i ,n ;
 printf("no. of elements");
-scanf("%d",&n);
+scanf("%d",&n); 
+//Input of number of elements
 for (i=0;i<n;i++){
     scanf("%d",&a[i]);
-
 }
+// Input of elements
 for(i=0;i<n;i++){
     printf("%d\t",a[i]);
 }
+// Printing elements
     return 0;
 }
--- insert.c
+++ insert.c
@@ -3,18 +3,25 @@ int main(){
     int a[25],i,n,num,ind;
     printf("Enter the number of elements: ");
     scanf("%d",&n);
+    // Input number of elements
     printf("Enter elements: ");
     for(i=0;i<n;i++){
         scanf("%d",&a[i]);
     }
+    // Input elements
     printf("Enter the number you want to insert: ");
     scanf("%d",&num);
+    // Input number that you want to insert
     printf("Enter the index: ");
+    // Input at which you want to insert the element
     scanf("%d",&ind);
     a[n] = a[ind];
+    // Making sure that current value stored at the desired index don't get lost
     a[ind] = num;
+    // Storing the desired number at desired index
     for(i=0;i<n+1;i++){
         printf("%d\t",a[i]);
     }
+    // Printing the updated array
     return 0;
 }
